eec84eadab1bd06cef31a8c4fc61c891d53ba467
victoriaehrlich <130980237+victoriaehrlich@users.noreply.github.com>
2023-05-11 10:49:32 +0100

    Initial Commit

---
+++ LewagonGIt/LewagonGItApp.swift
@@ -0,0 +1,17 @@
+//
+//  LewagonGItApp.swift
+//  LewagonGIt
+//
+//  Created by Victoria Ehrlich-Adám on 11.05.23.
+//
+
+import SwiftUI
+
+@main
+struct LewagonGItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
